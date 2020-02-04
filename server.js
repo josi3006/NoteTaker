4c77ec19b39cfbbd2c8ebc48d6856229c124e1c8
JGSims <josi3006@hotmail.com>
2020-02-04 18:30:25 -0500

    still not working

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const fs = require('fs');
+const util = require("util");
 const express = require('express');
 const path = require('path');
 
+
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
+const writeFileAsync = util.promisify(fs.writeFile);
+const readFileAsync = util.promisify(fs.readFile);
 
 let id = 1;
 
@@ -11,7 +16,10 @@ let noteBody = [];
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
 
 
 
@@ -26,22 +34,22 @@ app.listen(PORT, function () {
 // This code sends user to "notes" page when start button is clicked
 
 app.get('/notes', function (req, res) {
-    res.sendFile(path.join(__dirname, '/public/notes.html'));
+    res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
 
 // This code defaults to the "index" page
 
 app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, '/public/index.html'));
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 
 // This code adds the db to the path
 
-app.get('/api/notes', function (req, res) {
-    return res.sendFile(path.join(__dirname, "/db/db.json"));
-});
+// app.get('/api/notes', function (req, res) {
+//     return res.sendFile(path.join(__dirname, "/db/db.json"));
+// });
 
 
 
@@ -49,16 +57,11 @@ app.get('/api/notes', function (req, res) {
 // This code gets notes from the db
 
 app.get('/api/notes', function (req, res) {
-    try {
-        noteBody = fs.readFileSync('/db/db.json', 'utf8');
-        noteBody = JSON.parse(noteBody);
-        noteBody = JSON.stringify(noteBody);
-
-    } catch (err) {
-        console.log(err);
-    }
-
-    res.json(noteBody);
+    readFileAsync('./db/db.json', 'utf8')
+        .then(function (data) {
+            data = JSON.parse(data);
+            return res.json(data);
+        });
 
     console.log('This is not working:--------------------');
     console.log(noteBody);
@@ -67,31 +70,49 @@ app.get('/api/notes', function (req, res) {
 
 
 
+
+
+
 // This code writes notes to the db
 
 app.post('/api/notes', function (req, res) {
-    try {
-        noteBody = fs.readFileSync('./db/db.json', 'utf8');   // Reads current notes
 
-        noteBody = JSON.parse(noteBody);
-        req.body.id = noteBody.length + 1;
-        noteBody.push(req.body);
-        noteBody = JSON.stringify(noteBody);
+    let allNotes = req.body;
 
-        fs.writeFile('./db/db.json', noteBody, 'utf8', function (err) {      // Writes new note to db
-            if (err) throw err;
+    readFileAsync('./db/db.json', 'utf8')
+        .then(function (data) {
+
+            data = JSON.parse(data);
+            data.push(allNotes);
+
+            req.body.id = allNotes.length + 1;
+
+            writeFileAsync('./db/db.json', JSON.stringify(data));
+
         });
-
-        noteBody = JSON.parse(noteBody);
-        res.json(noteBody);
-
-        console.log('This one is working:--------------------');
-        console.log(noteBody);
-
-    } catch (err) {
-        throw err;
-    }
+    res.send('Done!');
 });
+
+
+
+
+
+
+
+// fs.writeFile('./db/db.json', noteBody, 'utf8', function (err) {      // Writes new note to db
+//     if (err) throw err;
+// });
+
+// noteBody = JSON.parse(noteBody);
+// res.json(noteBody);
+
+// console.log('This one is working:--------------------');
+// console.log(noteBody);
+
+// } catch (err) {
+//     throw err;
+// }
+// });
 
 
 
@@ -99,23 +120,40 @@ app.post('/api/notes', function (req, res) {
 // This code deletes notes
 
 app.delete('/api/notes/:id', function (req, res) {
-    try {
-        noteBody = fs.readFileSync('./db/db.json', 'utf8');
-        noteBody = JSON.parse(noteBody);
-        noteBody = noteBody.filter(function (note) {
-            return note.id != req.params.id;
-        });
-        noteBody = JSON.stringify(noteBody);
-        fs.writeFile('./db/db.json', noteBody, 'utf8', function (err) {
-            if (err) throw err;
-        });
+    let delNote = req.params.id;
 
-        res.send(JSON.parse(noteBody));
+        readFileAsync('./db/db.json', 'utf8')
+        .then(function(data) {
+            data = JSON.parse(data);
 
-    } catch (err) {
-        throw err;
-    }
-});
+            data.splce(delNote, 1);
+
+            for (var i = 0; i < data.length; i++) {
+                data[i].id = i;
+            }
+
+            writeFileAsync('./db/db.json', JSON.stringify(data));
+
+        });
+        res.send('Note was deleted!');
+
+    });
+
+//         noteBody = JSON.parse(noteBody);
+//         noteBody = noteBody.filter(function (note) {
+//             return note.id != req.params.id;
+//         });
+//         noteBody = JSON.stringify(noteBody);
+//         fs.writeFile('./db/db.json', noteBody, 'utf8', function (err) {
+//             if (err) throw err;
+//         });
+
+//         res.send(JSON.parse(noteBody));
+
+//     } catch (err) {
+//         throw err;
+//     }
+// });
 
 
 
